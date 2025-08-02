3515171d162e268ecdef51e23c7a961a55cb6737
Nenerox <andres.ps0308@gmail.com>
2025-08-02 09:03:33 -0600

    Se actualizo la visualizacion de los atributos

--- Jugador.java
+++ Jugador.java
@@ -1,10 +1,10 @@
 
 public class Jugador {
-    String nombre;
-    Pokemon pokemon1;
-    Pokemon pokemon2;
-    Pokemon pokemon3;
-    Pokemon pokemon4;
+    public String nombre;
+    public Pokemon pokemon1;
+    public Pokemon pokemon2;
+    public Pokemon pokemon3;
+    public Pokemon pokemon4;
 
     public Jugador(String nombre) {
         this.nombre = nombre;
